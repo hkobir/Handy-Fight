7c029f259b162c911a82366ec81948f50914544d
hkobir <hkobir10@gmail.com>
2019-10-01 16:17:17 +0600

    add splash screen

--- app/src/main/java/com/hk/rockpapersissor/MainActivity.java
+++ app/src/main/java/com/hk/rockpapersissor/MainActivity.java
@@ -206,4 +206,9 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+
+    @Override
+    public void onBackPressed() {
+        
+    }
 }
---
+++ app/src/main/java/com/hk/rockpapersissor/SplashActivity.java
@@ -0,0 +1,37 @@
+package com.hk.rockpapersissor;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+
+import com.hk.rockpapersissor.databinding.ActivitySplashBinding;
+
+public class SplashActivity extends AppCompatActivity {
+private ActivitySplashBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_splash);
+
+        //start animation
+        Animation animation= AnimationUtils.loadAnimation(SplashActivity.this,R.anim.rotate_clock);
+        binding.animateLayout.startAnimation(animation);
+        splash();
+    }
+
+    private void splash() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        },3000);
+
+    }
+}
